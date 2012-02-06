b30897617e538683d713395296ed05564b64e7fe
Scott McWhirter <scott.mcwhirter@joyent.com>
2012-02-05 22:43:21 -0800

    [QA-79] Ensure tap is outputted in a sane direction

--- runtests.sh
+++ runtests.sh
@@ -14,7 +14,7 @@ for tap_test in $LIST_OF_TESTS; do
     tap_out=$(echo $tap_out | sed 's#/#__#g')
     tap_out+=".tap"
     # we want to continue running through all tests even if some fail
-    node $tap_test > ./tap_output/$tap_out || true
+    ./node_modules/tap/bin/tap.js --tap $tap_test > ./tap_output/$tap_out || true
 done
 
 tar -czf tap_output.tgz tap_output/ 
