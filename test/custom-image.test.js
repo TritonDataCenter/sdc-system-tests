c1f8f07edd985759da005779daf315b004959dc8
Trent Mick <trentm@gmail.com>
2013-07-12 16:19:40 -0700

    RELENG-491: don't overload vars

--- test/custom-image.test.js
+++ test/custom-image.test.js
@@ -31,35 +31,35 @@ test('custom image (base 13.1.0)', function (t) {
     };
     var base_uuid = 'f669428c-a939-11e2-a485-b790efc0f0c1';
     var motd = 'booga booga';
-    var cmd = format('%s/mk-custom-image %s "%s" \'%s\'',
+    var cmd1 = format('%s/mk-custom-image %s "%s" \'%s\'',
         __dirname, base_uuid, motd, JSON.stringify(data));
     var options = {
         env: {
             //"TRACE": "1"
         }
     };
-    exec(cmd, options, function (err, stdout, stderr) {
-        t.ifError(err, format('error running "%s":\n'
+    exec(cmd1, options, function (err1, stdout1, stderr1) {
+        t.ifError(err1, format('error running "%s":\n'
             + '  err: %s\n'
             + '  stdout: %s\n'
-            + '  stderr: %s', cmd, err, stdout, stderr));
-        if (err) {
+            + '  stderr: %s', cmd1, err1, stdout1, stderr1));
+        if (err1) {
             return t.end();
         }
-        var customImageUuid = stdout.trim().split(/\n/g).slice(-1);
-        var cmd = format('%s/try-custom-image %s "%s"',
+        var customImageUuid = stdout1.trim().split(/\n/g).slice(-1);
+        var cmd2 = format('%s/try-custom-image %s "%s"',
             __dirname, customImageUuid, motd);
-        exec(cmd, function (err, stdout, stderr) {
-            t.ifError(err, format('error running "%s":\n'
+        exec(cmd2, function (err2, stdout2, stderr2) {
+            t.ifError(err2, format('error running "%s":\n'
                 + '  err: %s\n'
                 + '  stdout: %s\n'
-                + '  stderr: %s', cmd, err, stdout, stderr));
-            var cmd = 'sdc-imgadm delete ' + customImageUuid;
-            exec(cmd, function (err, stdout, stderr) {
-                t.ifError(err, format('error running "%s":\n'
+                + '  stderr: %s', cmd2, err2, stdout2, stderr2));
+            var cmd3 = 'sdc-imgadm delete ' + customImageUuid;
+            exec(cmd3, function (err3, stdout3, stderr3) {
+                t.ifError(err3, format('error running "%s":\n'
                     + '  err: %s\n'
                     + '  stdout: %s\n'
-                    + '  stderr: %s', cmd, err, stdout, stderr));
+                    + '  stderr: %s', cmd3, err3, stdout3, stderr3));
                 t.end();
             });
         });
