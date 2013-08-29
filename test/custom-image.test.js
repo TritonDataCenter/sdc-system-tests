70db6233996f050a07edaf90895a4d52fbe70306
John Sonnenschein <johns@joyent.com>
2013-08-29 13:25:20 -0700

    RELENG-498 tests for image creation and provisioning of kvm images (new package uuids)

--- test/custom-image.test.js
+++ test/custom-image.test.js
@@ -30,7 +30,8 @@ var data = {
 
 var base_uuid = 'f669428c-a939-11e2-a485-b790efc0f0c1';
 var base_kvm_uuid = '30e9e4c8-bbf2-11e2-ac3b-3b598ee13393';
-var restricted_package = '9b81f74c-7535-428f-91f6-92f7c5876a45';
+var restricted_package = '92e2b20a-0c37-11e3-9605-63a778146273';
+var kvm_restricted_package = 'a3501ccc-0c37-11e3-965d-ef7e825515c9';
 var six5server = '44454c4c-3700-104e-8033-cac04f475131';
 var old7server = '44454c4c-3700-104d-8032-cac04f475131';
 
@@ -148,8 +149,8 @@ test('custom image (base 13.1.0) on latest 7.0 CN with CloudAPI & DAPI', functio
 });
 
 test('custom KVM image (base CentOS6)', function(t) {
-    var cmd1 = format('%s/mk-custom-image %s "%s" \'%s\'',
-        __dirname, base_kvm_uuid, motd, JSON.stringify(data));
+    var cmd1 = format('%s/mk-custom-image %s "%s" \'%s\' %s',
+        __dirname, base_kvm_uuid, motd, JSON.stringify(data), kvm_restricted_package);
     var options = {
         //maxBuffer: 2*1024*1024,  // suggested if TRACE is on
         env: {
